7eb7c4c562f31d71550829cc657fa09230c70b83
Frank Schäckermann <15925142+fschaeck@users.noreply.github.com>
2022-09-13 09:41:17 +0200

    Finish annotations in files

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 with open(os.path.expanduser('~/somedir/mykeys.yaml'),encoding='UTF-8') as file:
     for item in file:
-        item=item.strip()
+        # strip the new-line characters \r and \n from the end of the line
+        item=item.rstrip('\r\n')
         print('ccc item is: ', str(item))
         if "var1" in item:
             print("Found var1")
